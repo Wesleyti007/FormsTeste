1716df75e592150860c4b685d2711933e4a0d4b3
wesley <wesley.ti07@gmail.com>
2021-03-14 18:31:32 -0300

    Alter tree

---
+++ Forms/Data/FormsContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Forms.Models;
+
+namespace Forms.Data
+{
+    public class FormsContext : DbContext
+    {
+        public FormsContext (DbContextOptions<FormsContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Forms.Models.Departament> Departament { get; set; }
+    }
+}
