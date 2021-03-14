72eeff837e19b32d6aa4ebe30090a28e4bcb343d
wesley <wesley.ti07@gmail.com>
2021-03-14 19:05:35 -0300

    Conexao com Banco de dados

--- Forms/Startup.cs
+++ Forms/Startup.cs
@@ -37,7 +37,8 @@ namespace Forms
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<FormsContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("FormsContext")));
+                    options.UseMySql(Configuration.GetConnectionString("FormsContext"), builder => 
+                    builder.MigrationsAssembly("Forms")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
